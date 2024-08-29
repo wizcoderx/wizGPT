eaa35569351c41e4e1722acda54cb75380fcec79
wizcoderx <wizcoderx@gmail.com>
2024-08-29 12:34:42 +0530

    Started working on the mongo part

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/api', methods=["GET","POST"])
+def qa():
+    if request.method == "GET":
+        data = {"result": "This is my first project created using chatgpt"}
+        return jsonify(data)
 
 app.run(debug=True)
